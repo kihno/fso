13abfd9149643d5e01cfe5b302a1c1193429390e
kihno <stistyle13@gmail.com>
2023-08-03 14:53:13 -0700

    Create POST endpoint to add patients with proper type validations

--- part9/patientor/backend/src/services/patientService.ts
+++ part9/patientor/backend/src/services/patientService.ts
@@ -1,5 +1,9 @@
+import { v1 as uuid } from 'uuid';
 import patientData from '../../data/patients';
-import { NonSensitivePatientInfo, Patient } from '../types';
+import { NonSensitivePatientInfo, Patient, NewPatient } from '../types';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const id: string = uuid();
 
 const getPatients = (): Patient[] => {
   return patientData;
@@ -15,7 +19,18 @@ const getNonSensitivePatients = (): NonSensitivePatientInfo[] => {
   })) ;
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+  const newPatient = {
+    id: id,
+    ...patient
+  };
+
+  patientData.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
-  getNonSensitivePatients
+  getNonSensitivePatients,
+  addPatient
 };
