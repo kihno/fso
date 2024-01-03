a4422d59598f36f7f3c9aa0bda243db71d9a28ef
kihno <stistyle13@gmail.com>
2024-01-02 16:57:20 -0800

    Add reset button to part7 anecdote create form

--- part7/routed-anecdotes/src/CreateNew.jsx
+++ part7/routed-anecdotes/src/CreateNew.jsx
@@ -16,23 +16,32 @@ const CreateNew = (props) => {
     })
   }
 
+  const handleReset = (e) => {
+    e.preventDefault();
+    
+    content.reset();
+    author.reset();
+    info.reset();
+  }
+
   return (
     <div>
       <h2>create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' type={content.type} value={content.value} onChange={content.onChange} />
+          <input {...content.props} />
         </div>
         <div>
           author
-          <input name='author' type={author.type} value={author.value} onChange={author.onChange} />
+          <input {...author.props} />
         </div>
         <div>
           url for more info
-          <input name='info' type={info.type} value={info.value} onChange={info.onChange} />
+          <input {...info.props} />
         </div>
-        <button>create</button>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>reset</button>
       </form>
     </div>
   )
